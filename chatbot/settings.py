953c6d638ad143a33db594437970fdec301ab6ed
Srivani <srivanireddyseelam@gmail.com>
2026-04-02 18:35:04 +0530

    Removed hardcoded SECRET_KEY and secured settings

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-40p3uvg2b_y*4)48kls$_-hmqrr*v_b6x$5l%a4x@sq^^)=ck$'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
